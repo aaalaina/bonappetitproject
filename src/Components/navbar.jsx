1c1ce09b1308522caffda11d9515349737096870
Alaina H <97alaina@gmail.com>
2022-08-10 16:56:21 -0400

    setting up components

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
-export default function navbar() {
+export default function Navbar() {
   return (
-    <div>navbar</div>
+    //carousel of images
+    //image source: https://www.pexels.com/search/food/
+    //might need another dependency for this
+    //will this fuck up merging...? time will tell
+
+
+    //import navbar component here I think? Or can we do it on the app.js file?
+
+    //two buttons (technically) but they'll be stylized a links
+
+    //I'm using flexbox for the hero and the 
+    <div></div>
   )
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>Home</div>
+  )
+}
