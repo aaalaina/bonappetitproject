910027d575d3bdbd66e17094280d29e16e925f3b
Administrator <bward6700@gmail.com>
2022-08-11 22:09:14 -0400

    shopping pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Shop from "./Pages/Shop";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Shop/>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const shopProducts = [
+    {
+        id: 1,
+        img: "https://assets.bonappetit.com/photos/62b4bfc1ac4f67616289d697/4:3/w_1600%2Cc_limit/0623-95pct-hot-sauce-lede.jpg",
+        title:"This Hot Sauce Is for Hot Sauce Lovers Who Dont Love Extreme Heat",
+        descr: "All of the flavor, none of the pain.",
+    },
+    {
+        id: 2,
+        img: "https://assets.bonappetit.com/photos/62bc52e25de14f53c5fce1fd/4:3/w_1600%2Cc_limit/0628-hr-piment-d-ville-lede.jpg",
+        title: "Piment dVille Chile Powder Is Smoky, Savory, and Important Super Fresh",
+        descr: "Forget stuffing your suitcase with Esplette, California makes it now too.",
+    },
+    {
+        id: 3,
+        img: "https://assets.bonappetit.com/photos/62b47a0f3b06e64020c59de5/4:3/w_1600%2Cc_limit/Whats-New-June.jpg",
+        title: "KitchenAids Sleek New Tea Kettle Wants to Boil Your Water Faster",
+        descr: "Plus: A pretty cast-iron pan from Our Place, cloud-inspired ceramics, and the spicy salt well be using all summer.",
+    },
+    {
+        id: 4,
+        img: "https://assets.bonappetit.com/photos/62b5db55086350b9114cb2df/4:3/w_1600%2Cc_limit/0624-cocktail-cherries-lede.jpg",
+        title:"Your Manhattans and Dirty Shirleys Need These Fancy Cocktail Cherries",
+        descr: "Garnish your favorite drink or, just eat them straight out of the jar. Best cocktail cherries for old-fashioneds.",
+    },
+    {
+        id: 5,
+        img: "https://assets.bonappetit.com/photos/62b5ec0e9f95eef7ff733c44/4:3/w_1600%2Cc_limit/0624-cinnamon-paxlovid-lede.jpg",
+        title: "This Candy Is the Only Thing That Helped My Terrible Paxlovid Mouth",
+        descr: "The antiviral treatment for COVID left a monstrous taste in my mouth. Cinnamon candies were my savior.",
+    },
+    {
+        id: 6,
+        img: "https://assets.bonappetit.com/photos/628bdf0f74566600d5d869a1/4:3/w_1600%2Cc_limit/0523-kyusu-teapot-lede.jpg",
+        title: "For a Tea Freak Like Me, Theres No Better Teapot Than a Kyusu",
+        descr: "This style of Japanese teapot is functional, beautifully designed, and doesnt drip. No tea bags required.",
+    },
+];
